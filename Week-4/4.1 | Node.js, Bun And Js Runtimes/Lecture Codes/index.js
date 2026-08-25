a6a44e2e57a3658690782a2c418473f69d82aa37
divyanshu <divyanshu473b@gmail.com>
2026-08-25 08:34:58 +0000

    Add [Week-4] 4.1 | Node.js Bun and Js Runtimes Lecture Codes

---
+++ Week-4/4.1 | Node.js, Bun And Js Runtimes/Lecture Codes/index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Hello world!"));
+console.log(chalk.redBright.bold("How are you doing"));
+console.log(chalk.green.underline("How are you hello"));
